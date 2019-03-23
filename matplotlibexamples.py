f9d8e85a3ccbb630232f79453898923610e8c44d
Jeremy D. Rogers <jdrogers@eddie>
2019-03-23 18:23:58 -0500

    cleaned up comments and added some info on where I borrowed snippets

--- matplotlibexamples.py
+++ matplotlibexamples.py
@@ -1,19 +1,25 @@
-# script to try out atom (and other potential python IDEs)
-# was useful in atom to install:
+# sandbox script to try out atom with python and plotting
+# Written (and snippets barrowed) by Jeremy D. Rogers <jdrogers5@wisc.edu>
+# To make atom more useful with python, install some community packages:
 # hydrogen
 # hydrogen-launcher
 # atom-ide-ui
 # ide-python
-# platformio-ide-terminal (super useful to have cli and, start with jupyter console --existing )
+# platformio-ide-terminal (super useful to have cli that shares the environment, start with: jupyter console --existing )
 # script
 #
-# see:
-# https://jstaf.github.io/2018/03/25/atom-ide.html
+# most of this taken from various howtos and guides:
 # https://realpython.com/python-matplotlib-guide/
+# https://towardsdatascience.com/the-art-of-effective-visualization-of-multi-dimensional-data-6c7202990c57
+
 #
 # use hydrogen setting inline or docked as default (requires restart)
 
-# %% first example
+# %% first example from
+# the double precent after the # makes this a cell for cell exexution using alt+shift+enter)
+# line at a time exceution with shift enter
+# note that plotting works best with cell execution, otherwise modfications to existing figure overwrite
+#
 import matplotlib.pyplot as plt
 import numpy as np
 np.random.seed(444)
@@ -36,6 +42,7 @@ fig.tight_layout()
 fig.show()
 fig.savefig('test2.svg')
 
+
 # %% second example
 
 x = np.random.randint(low=1, high=11, size=50)
@@ -54,8 +61,12 @@ ax2.legend(loc=(0.65, 0.8))
 ax2.set_title('Frequencies of $x$ and $y$')
 ax2.yaxis.tick_right()
 
-# %% third example
-import pandas as pd
+
+
+# %% third example using seaborn taken from screenshots in:
+# https://jstaf.github.io/2018/03/25/atom-ide.html
+
+# import pandas as pd
 import seaborn as sns
 tips = sns.load_dataset('tips')
 sns.lmplot(data=tips, x='total_bill', y='tip', hue='sex')
